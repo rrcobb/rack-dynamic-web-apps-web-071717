c70bc83b5f7f1a0c903d12f30f5394a56510ae62
Rob Cobb <rwcobbjr@gmail.com>
2018-03-28 15:41:00 -0400

    Done.

--- app/application.rb
+++ app/application.rb
@@ -2,7 +2,14 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, World"
+    x = Kernel.rand(1..20)
+    y = Kernel.rand(1..20)
+    z = Kernel.rand(1..20)
+    if x == y && x == z
+      resp.write "You Win"
+    else
+      resp.write "You Lose"
+    end
     resp.finish
   end
 
